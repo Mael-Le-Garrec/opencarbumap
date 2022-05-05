4f2de198531ff3be74473f19c611876b8af55232
Maël Le Garrec <mael@legarrec.org>
2022-05-05 12:08:23 +0200

    Refactor download data

--- download_data.sh
+++ download_data.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+data_type=instantane
 
-wget https://donnees.roulez-eco.fr/opendata/instantane
-unzip jour
-rm jour
+wget https://donnees.roulez-eco.fr/opendata/$data_type
+unzip $data_type
+rm $data_type
+
 mv PrixCarburants* data.xml
